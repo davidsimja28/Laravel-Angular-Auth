ce5f16e603ca0354fdb3beee40cb1c0ff51d7587
David <davidsimja28@gmail.com>
2020-05-14 20:33:10 -0500

    Created Components

--- FrontendLimpio/src/app/app.module.ts
+++ FrontendLimpio/src/app/app.module.ts
@@ -5,6 +5,12 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { APP_ROUTING } from './app.routes';
 import { HomeComponent } from './components/home/home.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { LoginComponent } from './components/login/login.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { PerfilComponent } from './components/perfil/perfil.component';
+import { RequestResetComponent } from './components/password/request-reset/request-reset.component';
+import { ResponseResetComponent } from './components/password/response-reset/response-reset.component';
 
 
 
@@ -12,6 +18,12 @@ import { HomeComponent } from './components/home/home.component';
   declarations: [
     AppComponent,
     HomeComponent,
+    NavbarComponent,
+    LoginComponent,
+    SignupComponent,
+    PerfilComponent,
+    RequestResetComponent,
+    ResponseResetComponent,
     
 
   ],
--- FrontendLimpio/src/app/app.routes.ts
+++ FrontendLimpio/src/app/app.routes.ts
@@ -1,6 +1,12 @@
 import { RouterModule, Routes, Route } from '@angular/router';
 
 import { HomeComponent } from './components/home/home.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { LoginComponent } from './components/login/login.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { PerfilComponent } from './components/perfil/perfil.component';
+import { RequestResetComponent } from './components/password/request-reset/request-reset.component';
+import { ResponseResetComponent } from './components/password/response-reset/response-reset.component';
 
 
 
@@ -10,6 +16,13 @@ const APP_ROUTES: Routes = [
     {path: 'home', component: HomeComponent},  
     {path: '', component: HomeComponent, pathMatch: 'full' },
     {path: '**', pathMatch: 'full' , redirectTo: 'menu' },
+    {path: 'navbar', component: NavbarComponent},
+    {path: 'login', component: LoginComponent},
+    {path: 'signup', component: SignupComponent},
+    {path: 'perfil', component: PerfilComponent},
+    {path: 'request', component: RequestResetComponent},
+    {path: 'response', component: ResponseResetComponent},
+
 
     
 
