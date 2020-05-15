0b16662e0d3cb9bc5d08dc66dc5d67f14ee7dbf3
David <davidsimja28@gmail.com>
2020-05-14 21:16:21 -0500

    JWT config

--- backend/app/User.php
+++ backend/app/User.php
@@ -4,26 +4,32 @@ namespace App;
 
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+ //Añadimos la clase JWTSubject 
+ use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class User extends Authenticatable
+class User extends Authenticatable implements JWTSubject
 {
     use Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
+
     protected $fillable = [
         'name', 'email', 'password',
     ];
 
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
+
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    /*
+            Añadiremos estos dos métodos
+        */
+        public function getJWTIdentifier()
+        {
+            return $this->getKey();
+        }
+        public function getJWTCustomClaims()
+        {
+            return [];
+        }
 }
